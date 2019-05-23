33990a25ffffc8c45e84537bbb97ba56fb57c685
Daniel Paiva <daniel.paiva@exaud.com>
2019-05-23 15:01:20 +0100

    Fragment has a button!

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.fragments;
 
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentManager managerBlankFragment = getSupportFragmentManager();
+        FragmentTransaction transactionBlankFragment = managerBlankFragment.beginTransaction();
+
+        transactionBlankFragment.add(R.id.fragment_blank, new BlankFragment());
+        transactionBlankFragment.commit();
+/*
+        Fragment fragment = managerBlankFragment.findFragmentById(R.id.fragment_blank);
+        transactionBlankFragment.remove(fragment);
+        transactionBlankFragment.commit();
+*/
     }
 }
