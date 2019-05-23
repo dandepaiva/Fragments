ec07989629a50e6d86dad7f833d6962de4f26b09
Daniel Paiva <daniel.paiva@exaud.com>
2019-05-23 14:33:14 +0100

    it works!.......?

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.fragments;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String FRAGMENT_MESSAGE = "com.example.fragments.FRAGMENT1";
 
-
-    //yep yop yup
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
